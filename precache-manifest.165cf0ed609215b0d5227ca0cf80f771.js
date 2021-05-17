c07b1040ecb841bc784e1bb69ce63bf366e4b6b1
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-17 14:17:25 -0300

    Updates

---
+++ precache-manifest.165cf0ed609215b0d5227ca0cf80f771.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3983359e7a5736b0f6d4049d7922d6",
+    "url": "/planets-starwars/index.html"
+  },
+  {
+    "revision": "bbfe2a120720c45386c0",
+    "url": "/planets-starwars/static/css/main.4301dd7a.chunk.css"
+  },
+  {
+    "revision": "ee4d5e3e930a93d3c5ea",
+    "url": "/planets-starwars/static/js/2.bcfdcb52.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/planets-starwars/static/js/2.bcfdcb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfe2a120720c45386c0",
+    "url": "/planets-starwars/static/js/main.965e6ed3.chunk.js"
+  },
+  {
+    "revision": "5c4c56f7d17462ee5653",
+    "url": "/planets-starwars/static/js/runtime-main.2bdad7b3.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/planets-starwars/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/planets-starwars/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "021c73526847c1257ae885a96f817b78",
+    "url": "/planets-starwars/static/media/darthVaden.021c7352.jpg"
+  }
+]);
