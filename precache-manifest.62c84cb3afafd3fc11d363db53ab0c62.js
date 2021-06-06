2bdf4d97bad5d901d5267fe20a4ba41c4c83a00d
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-06-05 22:23:06 -0300

    Updates

---
+++ precache-manifest.62c84cb3afafd3fc11d363db53ab0c62.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b92f4e17a964413afb0bf113bac888",
+    "url": "/planets-starwars/index.html"
+  },
+  {
+    "revision": "52292842152b1754ba1d",
+    "url": "/planets-starwars/static/css/main.63b6b875.chunk.css"
+  },
+  {
+    "revision": "ef38d8f5870e732b9050",
+    "url": "/planets-starwars/static/js/2.5e9157c7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/planets-starwars/static/js/2.5e9157c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52292842152b1754ba1d",
+    "url": "/planets-starwars/static/js/main.c3423621.chunk.js"
+  },
+  {
+    "revision": "5c4c56f7d17462ee5653",
+    "url": "/planets-starwars/static/js/runtime-main.2bdad7b3.js"
+  },
+  {
+    "revision": "d890eeba2e46c4f7ad19a62c2ec5e40a",
+    "url": "/planets-starwars/static/media/Adi Gallia.d890eeba.jpg"
+  },
+  {
+    "revision": "9b9887610c0940a8ca92816b10175038",
+    "url": "/planets-starwars/static/media/Alderaan.9b988761.jpg"
+  },
+  {
+    "revision": "929ded25b6002b65125c5a909c695c58",
+    "url": "/planets-starwars/static/media/Anakin Skywalker.929ded25.jpg"
+  },
+  {
+    "revision": "de27d4db58c03cd0cc1160403969f44f",
+    "url": "/planets-starwars/static/media/Bail Prestor Organa.de27d4db.jpg"
+  },
+  {
+    "revision": "d340d63dc5eb95dc9e25c783b581e971",
+    "url": "/planets-starwars/static/media/Bespin.d340d63d.jpg"
+  },
+  {
+    "revision": "75328c5f23199b79662784c4ad72a09b",
+    "url": "/planets-starwars/static/media/Biggs Darklighter.75328c5f.jpg"
+  },
+  {
+    "revision": "8509f04acf05c07c7cb6279f874e5f49",
+    "url": "/planets-starwars/static/media/C-3PO.8509f04a.jpg"
+  },
+  {
+    "revision": "a488248516157e29d9bc766c87f13f68",
+    "url": "/planets-starwars/static/media/Cliegg Lars.a4882485.jpg"
+  },
+  {
+    "revision": "aae7a97bb7feda9478df29154083bca0",
+    "url": "/planets-starwars/static/media/Cordé.aae7a97b.jpg"
+  },
+  {
+    "revision": "d26ebbef08b256d8134359e71747519c",
+    "url": "/planets-starwars/static/media/Coruscant.d26ebbef.jpg"
+  },
+  {
+    "revision": "58795608d50457c340e76d763e73c8d5",
+    "url": "/planets-starwars/static/media/Dagobah.58795608.jpg"
+  },
+  {
+    "revision": "fdfc23109c447ed82d9c3cd17b18c51c",
+    "url": "/planets-starwars/static/media/Darth Vader.fdfc2310.jpg"
+  },
+  {
+    "revision": "672e212fa7f0a34a92c4709f656088fb",
+    "url": "/planets-starwars/static/media/Dormé.672e212f.jpg"
+  },
+  {
+    "revision": "a9c7b1f33588f9ae14c5ace036508ec6",
+    "url": "/planets-starwars/static/media/Endor.a9c7b1f3.jpg"
+  },
+  {
+    "revision": "46d624e2f5dc6ed5303ae21f5e19f830",
+    "url": "/planets-starwars/static/media/Finis Valorum.46d624e2.jpg"
+  },
+  {
+    "revision": "f87754467b916b9fe6f67d3a97218b24",
+    "url": "/planets-starwars/static/media/Gregar Typho.f8775446.jpg"
+  },
+  {
+    "revision": "b4c3d4adb84488f57a88295b9318d8f5",
+    "url": "/planets-starwars/static/media/Hoth.b4c3d4ad.jpg"
+  },
+  {
+    "revision": "3249c0bc0f67ff62595130410257581b",
+    "url": "/planets-starwars/static/media/Jar Jar Binks.3249c0bc.jpg"
+  },
+  {
+    "revision": "386b268e751d8999684ae72cc90f7f07",
+    "url": "/planets-starwars/static/media/Jocasta Nu.386b268e.jpg"
+  },
+  {
+    "revision": "637c8c9310334cd4b57fcf73922a9707",
+    "url": "/planets-starwars/static/media/Kamino.637c8c93.jpg"
+  },
+  {
+    "revision": "50f7c8e7b7dfefd5ac19f275d03391d9",
+    "url": "/planets-starwars/static/media/Lama Su.50f7c8e7.jpg"
+  },
+  {
+    "revision": "6ec22f44db0cf13d65bc107398010074",
+    "url": "/planets-starwars/static/media/Leia Organa.6ec22f44.jpg"
+  },
+  {
+    "revision": "e2cc4993f0e097d7b9ceeef8d99a15d8",
+    "url": "/planets-starwars/static/media/Lobot.e2cc4993.jpg"
+  },
+  {
+    "revision": "10fdd3455c01c63cc13634d01859a27f",
+    "url": "/planets-starwars/static/media/Luke Skywalker.10fdd345.jpg"
+  },
+  {
+    "revision": "aef4c3c3437c921f2df8a25f272e5cb1",
+    "url": "/planets-starwars/static/media/Naboo.aef4c3c3.jpg"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/planets-starwars/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "65ca5f6a769e929d741c167dd89fcc2d",
+    "url": "/planets-starwars/static/media/Owen Lars.65ca5f6a.jpg"
+  },
+  {
+    "revision": "32240b9627bf29a009ffac762560894c",
+    "url": "/planets-starwars/static/media/Padmé Amidala.32240b96.jpg"
+  },
+  {
+    "revision": "27683bfd844e3512483fb1f95479d441",
+    "url": "/planets-starwars/static/media/Palpatine.27683bfd.jpg"
+  },
+  {
+    "revision": "fd1a7604303283bfd322abff79385a46",
+    "url": "/planets-starwars/static/media/R2-D2.fd1a7604.jpg"
+  },
+  {
+    "revision": "0dd6c3b6686ee43c2aa5625de8c32a80",
+    "url": "/planets-starwars/static/media/R5-D4.0dd6c3b6.jpg"
+  },
+  {
+    "revision": "42775d571c547f981cb27a8e98a6cc96",
+    "url": "/planets-starwars/static/media/Raymus Antilles.42775d57.jpg"
+  },
+  {
+    "revision": "a7b079c05ee2e5e7fdd55b3d2067ccff",
+    "url": "/planets-starwars/static/media/Ric Olié.a7b079c0.jpg"
+  },
+  {
+    "revision": "35e73d6e2145996a0ea1952e0e7d3e0b",
+    "url": "/planets-starwars/static/media/Roos Tarpals.35e73d6e.jpg"
+  },
+  {
+    "revision": "3e7f187744b5d26389685d6f766bea3b",
+    "url": "/planets-starwars/static/media/Rugor Nass.3e7f1877.jpg"
+  },
+  {
+    "revision": "8569ca3ca1683765fa6f613aa0b307a3",
+    "url": "/planets-starwars/static/media/Shmi Skywalker.8569ca3c.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/planets-starwars/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "3840e6c8c024256d3c01103af6aa430c",
+    "url": "/planets-starwars/static/media/Tatooine.3840e6c8.jpg"
+  },
+  {
+    "revision": "fbcd0085bc9b52dfbaa35f3b8196ad18",
+    "url": "/planets-starwars/static/media/Taun We.fbcd0085.jpg"
+  },
+  {
+    "revision": "2b9e06ebb195490329b0c0cb419ad8b1",
+    "url": "/planets-starwars/static/media/Wicket Systri Warrick.2b9e06eb.jpg"
+  },
+  {
+    "revision": "296e8ebce0d9c636d40c0ad78b8aca26",
+    "url": "/planets-starwars/static/media/Yavin IV.296e8ebc.jpg"
+  },
+  {
+    "revision": "021c73526847c1257ae885a96f817b78",
+    "url": "/planets-starwars/static/media/darthVaden.021c7352.jpg"
+  },
+  {
+    "revision": "ed2c53adb3d5433f2e21882158ba20dc",
+    "url": "/planets-starwars/static/media/header.ed2c53ad.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/planets-starwars/precache-manifest.bde86f311add19636df0acf3c868357c.js"
+  "/planets-starwars/precache-manifest.62c84cb3afafd3fc11d363db53ab0c62.js"
 );
 
 self.addEventListener('message', (event) => {
